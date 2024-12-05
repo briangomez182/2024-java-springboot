8cc802f6424efb462fd89ef03e9f3b0a70274c1e
briangomez182 <briangomezz182@gmail.com>
2024-12-05 05:04:11 -0300

    Commit rápido de Prueba

--- borrar/src/main/java/org/example/Main.java
+++ borrar/src/main/java/org/example/Main.java
@@ -12,19 +12,33 @@ public class Main {
         // Servicio CRUD
         MovieService service = new MovieService();
 
-        //Create
-        Movie newMovie = new Movie();
-        newMovie.setTitle("Bambi");
-        newMovie.setAwards(7);
-        newMovie.setRating(2.5);
-        newMovie.setRelease_date(LocalDateTime.of(2000,1,1,0,0));
-        newMovie.setLength(120);
+        // TODO: 01/12/2024         Create
+        Movie newMovie = new Movie(null, "Alberto", 6.5, 7, LocalDateTime.of(1990,2,5,0,0), 110);
+       /* newMovie.setTitle("Juan");
+        newMovie.setRating(6.5);
+        newMovie.setAwards(6);
+        newMovie.setRelease_date(LocalDateTime.of(1990,1,1,0,0));
+        newMovie.setLength(120);*/
 
         service.create(newMovie);
 
-        // READ: Find all
-       /* List<Movie> movies = service.findAll();
-        System.out.println("All Products:");
-        movies.forEach( movie -> System.out.println(movie.toString()));*/
+        // TODO: 01/12/2024         FindByPK
+      /*  Movie findMovie = service.findById(10);
+        System.out.println("Found Product: " + findMovie.toString());*/
+
+        // TODO: 01/12/2024         Find all
+      /*  List<Movie> movies = service.findAll();
+        for (Movie pelicula: movies ) {
+            System.out.println(pelicula.toString());
+        }*/
+
+        // TODO: 01/12/2024         Update
+      /*  Movie findMovieUpdate = service.findById(26);
+        findMovieUpdate.setTitle("Nuevo titulo");
+        service.update(findMovieUpdate);*/
+
+        // TODO: 01/12/2024         Delete
+        service.delete(25);
+
     }
 }
